ca72bd5c7aef69ff58cb965c03853c9de2f45617
owner <owner1147@gmail.com>
2025-08-09 13:01:05 +0300

    install PrimeVue

--- src/main.js
+++ src/main.js
@@ -1,11 +1,22 @@
-import './assets/main.css'
+import './assets/css/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
+import Button from 'primevue/button'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+})
+
+// Глобальна реєстрація Button компонента
+app.component('Button', Button)
 
 app.mount('#app')
